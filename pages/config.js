0137ed2b71142cd83396bc339c0a31cffaf77c6f
James Trimble <jtrim.ons@gmail.com>
2024-01-21 15:36:00 +0000

    Add other fossil category, and tweak nuclear colour

--- pages/config.js
+++ pages/config.js
@@ -1,8 +1,9 @@
 export const fuelGroupToColour = ([
-  {group: "Fossil fuels", colour: "#333333"},
-  {group: "Nuclear", colour: "#9467bd"},
-  {group: "Hydro", colour: "#1f77b4"},
-  {group: "Wind", colour: "#2ca02c"},
-  {group: "Solar", colour: "#ff7f0e"},
-  {group: "Other", colour: "#f4da00"}
+  { group: "Coal", colour: "#333333" },
+  { group: "Other fossil fuels", colour: "#aaa" },
+  { group: "Nuclear", colour: "#a774d5" /*"#9467bd"*/ },
+  { group: "Hydro", colour: "#1f77b4" },
+  { group: "Wind", colour: "#2ca02c" },
+  { group: "Solar", colour: "#ff7f0e" },
+  { group: "Other", colour: "#f4da00" }
 ]);
